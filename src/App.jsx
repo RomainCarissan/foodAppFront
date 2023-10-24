4f077e81d0d2af87009d576cbca8bedf1aeabe8b
romain.carissan2@gmail.com <romain.carissan2@gmail.com>
2023-10-24 10:10:11 +0200

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import ReceipePage from "./Pages/ReceipePage/ReceipePage";
 import FavPage from "./Pages/FavPage/FavPage";
 import CreatePage from "./Pages/CreatePage/CreatePage";
 import Navbar from "./components/NavBar";
+import ConnexionPage from "./Pages/ConnexionPage/ConnexionPage";
 
 function App() {
   return (
@@ -19,9 +20,10 @@ function App() {
         <Route path="/search" element={<SearchPage />} />
         <Route path="/search/:category" element={<FilterPage />} />
         <Route path="/search/area/:area" element={<FilterCountryPage />} />
-        <Route path="/meals/:mealID" element={<ReceipePage />} />
+        <Route path="/meals/:id" element={<ReceipePage />} />
         <Route path="/favorites" element={<FavPage />} />
         <Route path="/create" element={<CreatePage />} />
+        <Route path="/connexion" element={<ConnexionPage />} />
       </Routes>
     </>
   );
