557bdc1207ae6374fa301f20a1cf347de25dcc06
Victor Duveau <vic.duveau@hotmail.fr>
2023-10-24 09:34:56 +0200

    Update app,jsx with ReceipePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import { Routes, Route, useParams } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./Pages/HomePage/HomePage";
 import SearchPage from "./Pages/SearchPage/SearchPage";
 import FilterPage from "./Pages/FilterPage/FilterPage";
-import FilterCountryPage from "./Pages/FilterCountryPage/FilterCountryPage";
 import ReceipePage from "./Pages/ReceipePage/ReceipePage";
 import FavPage from "./Pages/FavPage/FavPage";
 import CreatePage from "./Pages/CreatePage/CreatePage";
 import Navbar from "./components/NavBar";
+import ConnexionPage from "./Pages/ConnexionPage/ConnexionPage";
 
 function App() {
   return (
@@ -17,11 +17,11 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchPage />} />
-        <Route path="/search/:category" element={<FilterPage />} />
-        <Route path="/search/area/:area" element={<FilterCountryPage />} />
-        <Route path="/meals/:mealID" element={<ReceipePage />} />
+        <Route path="/search/:filter" element={<FilterPage />} />
+        <Route path="/meals/:id" element={<ReceipePage />} />
         <Route path="/favorites" element={<FavPage />} />
         <Route path="/create" element={<CreatePage />} />
+        <Route path="/connexion" element={<ConnexionPage />} />
       </Routes>
     </>
   );
