2933795d3d24d6b814d8d8295bcda84d7b68bf57
Victor Duveau <vic.duveau@hotmail.fr>
2023-10-25 18:11:09 +0200

    update app, recipePage, connexionPage, RegisterPage, favPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import HomePage from "./Pages/HomePage/HomePage";
 import SearchPage from "./Pages/SearchPage/SearchPage";
 import FilterPage from "./Pages/FilterPage/FilterPage";
 import ReceipePage from "./Pages/ReceipePage/ReceipePage";
-import FavPage from "./Pages/FavPage/FavPage";
+import FavPage from "./Pages/FavPage/favPage";
 import CreatePage from "./Pages/CreatePage/CreatePage";
 import Navbar from "./components/NavBar";
 import ConnexionPage from "./Pages/ConnexionPage/ConnexionPage";
+import RegisterPage from "./Pages/RegisterPage/RegisterPage";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/favorites" element={<FavPage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/connexion" element={<ConnexionPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </>
   );
--- src/Pages/FavPage/favPage.jsx
+++ src/Pages/FavPage/favPage.jsx
@@ -1,7 +1,91 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-function FavPage() {
-  return <div>FavPage</div>;
+function favPage() {
+  const [meals, setMeals] = useState(null);
+  const [favMember, setFavMember] = useState(null);
+  let arrFav = [];
+  let userId = localStorage.getItem("id");
+  /* const [search, setSearch] = useState(""); */
+  console.log(userId);
+  async function fetchMeals() {
+    try {
+      const response = await axios.get("https://foodapp.adaptable.app/meals");
+      setMeals(response.data);
+      const answer = await axios.get(
+        "https://foodapp.adaptable.app/members/" + userId
+      );
+      console.log(response.data);
+      // setFavMember(answer.data[userId].favorite);
+      // const fav = answer.data[0].favorite;
+      // const user = answer.data.find((user) => user.id === userId);
+      const user = answer.data;
+      if (user) {
+        setFavMember(user.favorite);
+      } else {
+        console.log("User not found");
+      }
+      console.log(setMeals);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchMeals();
+  }, []);
+
+  if (!meals) {
+    return <p>No connected yet</p>;
+  }
+
+  if (!favMember) {
+    return <p>No Favories yet</p>;
+  }
+
+  {
+    favMember.map((fav) => {
+      return arrFav.push(fav.id);
+    });
+  }
+  console.log(arrFav);
+  /* console.log("meal", meals); */
+
+  return (
+    <>
+      <div className="all-meals">
+        <div className="all-meals">
+          {arrFav.map((idmeal) =>
+            meals.map((meal) => (
+              <React.Fragment key={meal.id}>
+                {Number(idmeal) === Number(meal.id) ? (
+                  <Link
+                    key={meal.id}
+                    to={`/meals/${meal.id}`}
+                    data-hidden={meal.data_hidden ? meal.data_hidden : "false"}
+                  >
+                    {/* {console.log(member)} */}
+                    <div className="meal">
+                      <div className="meal-img">
+                        <img src={meal.image} alt={`${meal.name} Image`} />
+                      </div>
+                      <div className="meal-info">
+                        <h3>{meal.name}</h3>
+                      </div>
+                    </div>
+                  </Link>
+                ) : (
+                  <p> </p>
+                )}
+                {/* {console.log(idmeal)} */}
+              </React.Fragment>
+            ))
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default FavPage;
+export default favPage;
